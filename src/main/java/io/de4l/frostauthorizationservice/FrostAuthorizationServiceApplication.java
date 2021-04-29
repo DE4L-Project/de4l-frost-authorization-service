752fd09d6fc20ae8d9f5a27ab194f5e097919f01
gaunitz <gaunitz@wifa.uni-leipzig.de>
2021-04-29 11:17:01 +0200

    - init commit

---
+++ src/main/java/io/de4l/frostauthorizationservice/FrostAuthorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package io.de4l.frostauthorizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrostAuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrostAuthorizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/de4l/frostauthorizationservice/FrostAuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.de4l.frostauthorizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrostAuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
